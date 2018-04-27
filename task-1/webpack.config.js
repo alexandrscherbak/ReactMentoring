a92d4b41c25d593b6d1c55a4a086f11371dd32be
Aliaksandr Shcharbak <aliaksandr.shcharbak@thomsonreuters.com>
2018-04-27 14:33:32 +0300

    implement task 2 (webpack)

--- task-1/webpack.config.js
+++ task-1/webpack.config.js
@@ -1,35 +1,52 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve('dist'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{ test: /\.css$/,
-				use: [
-					{ loader: "style-loader" },
-					{ loader: "css-loader" }
-				]
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: "babel-loader"
-			}, {
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				use: "babel-loader"
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'src/index.html',
-			inject: 'body'
-		}),
-	]
+module.exports = function (env, options) {
+	const isProduction = options.mode === "production";
+
+	const config = {
+		context: path.join(__dirname, 'src'),
+		entry: './index.js',
+		mode: isProduction ? 'production' :'development',
+		devtool: isProduction ? 'none' : 'eval-source-map',
+
+		resolve: {
+			extensions: ['.js', '.jsx']
+		},
+
+		output: {
+			path: path.resolve('dist'),
+			filename: 'bundle.js'
+		},
+
+		module: {
+			rules: [
+				{
+					test: /\.css$/,
+					use: [
+						{ loader: 'style-loader' },
+						{ loader: 'css-loader' }
+					]
+				},
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: 'babel-loader'
+				}, {
+					test: /\.jsx?$/,
+					exclude: /node_modules/,
+					use: 'babel-loader'
+				}
+			]
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: 'index.html',
+				inject: 'body',
+				title: 'ReactApp',
+			}),
+		],
+	};
+
+	return config;
 }
